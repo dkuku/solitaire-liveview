ebb27c178169ef935ff9763f21ab731c55db630a
Daniel Kukula <daniel.kuku@gmail.com>
2019-11-26 20:01:36 +0000

    refactor render functions

--- lib/lvsolitaire_web/live/solitaire_live.ex
+++ lib/lvsolitaire_web/live/solitaire_live.ex
@@ -1,4 +1,7 @@
 defmodule LVSolitaireWeb.LVSolitaireLive do
+  @moduledoc """
+  Frontend logic
+  """
   use Phoenix.LiveView
   alias Solitaire.Game, as: Game
   alias Solitaire.Foundation, as: Foundation
@@ -8,7 +11,8 @@ defmodule LVSolitaireWeb.LVSolitaireLive do
   alias Solitaire.Cards, as: Cards
   require Logger
 
-  @ranks [:a,2,3,4,5,6,7,8,9,10,:j,:q,:k]
+  @ranks [:a, 2, 3, 4, 5, 6, 7, 8, 9, 10, :j, :q, :k]
+
   def num_to_rank(rank), do: Enum.at(@ranks, rank - 1)
   def mount(_session, socket) do
     game = Deck.new
@@ -18,115 +22,16 @@ defmodule LVSolitaireWeb.LVSolitaireLive do
     socket = assign(socket, :clicked, nil)
     {:ok, socket}
   end
-  
-  #{
-#  {[stos-ukryte],[stos-widoczne]},
-#  [
-#    {[ukryte],[widoczne]},
-#    ...
-#  ],
-#  [[wiadoczne], [ wodoczne ]]
-#
-#}
-  def focus?(%{clicked: nil}, _), do: ""
-  def focus?(%{clicked: clicked}, card) do
-    if clicked == card do
-      "focus"
-    else
-      ""
-    end
-  end
-  def render_card(assigns, pile_type, {suit, rank} = card, index \\ nil) do 
-    ~L"""
-    <li>
-    <div phx-click="click"
-    phx-value-suit="<%= "#{suit}" %>"
-    phx-value-rank="<%= "#{rank}" %>"
-    phx-value-pile="<%= "#{pile_type}" %>"
-    phx-value-index="<%= "#{index}" %>"
-    class="card <%= focus?(assigns, card) %> rank-<%= num_to_rank( rank ) %> <%= suit %>">
-        <span class="rank"><%= num_to_rank(rank) %></span>
-        <span class="suit">&<%= suit %>;</span>
-      </div>
-    </li>
-    """
-  end
-  def render_card(assigns, :deck, :back, _) do 
-    ~L"""
-    <li><div phx-click="click-deck" class="card back">*</div></li>
-    """
-  end
-  def render_card(assigns, :tableau, :back, _) do 
-    ~L"""
-    <li><div class="card back">*</div></li>
-    """
-  end
-  def render_card(assigns, :foundation, _, index) do 
-    ~L"""
-    <li><div phx-click="click-empty"
-             phx-value-pile="foundation"
-             phx-value-index="<%= "#{index}" %>"
-             class="card">
-    </div></li>
-    """
-  end
-  def render_card(assigns, _, _, _) do 
-    ~L"""
-    <li><div class="card"></div></li>
-    """
-  end
 
   def render(assigns) do
-    {reserve,tableau,foundation } = assigns.game
-    {deck,waste} = reserve
-    ~L"""
-    <center>
-      <div class="playingCards inline">
-        <ul class="deck inline">
-          <%= if deck do %>
-            <%= render_card(assigns, :deck, :back) %>
-          <% end %>
-        </ul>
-      </div>
-
-      <div class="playingCards inline">
-        <ul class="deck inline">
-          <%= for {suit, rank} = card  <- Enum.reverse(waste) do %>
-            <%= render_card(assigns, :deck, card, 0) %>
-          <% end %>
-        </ul>
-      </div>
-
-      <%= for {foundation, idx}  <- Enum.with_index(foundation) do %>
-        <div class="playingCards inline">
-          <ul class="deck inline">
-          <%= render_card(assigns, :foundation, nil) %>
-          <%= for {suit, rank} = card <- Enum.reverse(foundation) do %>
-            <%= render_card(assigns, :foundation, card, idx) %>
-          <% end %>
-          </ul>
-        </div>
-      <% end %>
-    </center>
-
-    <center phx-click="unselect">
-      <%= for {{invisible, visible}, idx}  <- Enum.with_index(tableau) do %>
-        <div class="playingCards inline">
-          <ul class="tableau">
-            <%= for card  <- invisible do %>
-              <%= render_card(assigns, :tableau, :back, idx) %>
-            <% end %>
-            <%= for {suit, rank} = card  <- Enum.reverse(visible) do %>
-              <%= render_card(assigns, :tableau, card, idx) %>
-            <% end %>
-          </ul>
-        </div>
-      <% end %>
-    </center>
-    """
+    LVSolitaireWeb.GameView.render("lv_solitaire.html", assigns)
   end
 
+  def focus?(%{clicked: clicked}, clicked), do: "focus"
+  def focus?(%{clicked: _}, _), do: ""
+
   #TODO need this when multiple moves possible for A
+  #TODO fix when moveng card valid card to different pile
   def handle_event("click-empty", params, socket) do
     IO.inspect(params)
     clicked = socket.assigns.clicked
@@ -147,15 +52,25 @@ defmodule LVSolitaireWeb.LVSolitaireLive do
     end
   end
   def handle_event("click", params, socket) do
-    IO.inspect(params)
-    clicked = socket.assigns.clicked
+    handle_clicked(params, socket, socket.assigns.clicked)
+  end
+  def handle_clicked(params, socket, nil) do
     moves = socket.assigns.game
           |> Game.possible_moves()
-          |> IO.inspect()
-          |> filter_by_clicked(params, clicked)
-          |> IO.inspect()
           |> check_possible_moves(params)
-          |> IO.inspect()
+    if len(moves) == 1 do
+      socket = assign(socket, :game, Game.perform(socket.assigns.game, Enum.fetch!(moves, 0)))
+      socket = assign(socket, :clicked, nil)
+      {:noreply, socket}
+    else
+      socket = assign(socket, :clicked, format_card(params))
+      {:noreply, socket}
+    end
+  end
+  def handle_clicked(params, socket, clicked) do
+    moves = socket.assigns.game
+          |> Game.possible_moves()
+          |> filter_by_clicked(params, clicked)
     if len(moves) == 1 do
       socket = assign(socket, :game, Game.perform(socket.assigns.game, Enum.fetch!(moves, 0)))
       socket = assign(socket, :clicked, nil)
@@ -166,8 +81,13 @@ defmodule LVSolitaireWeb.LVSolitaireLive do
     end
   end
   def handle_event("click-deck", _, socket) do
-    socket = assign(socket, :game, Game.turn(socket.assigns.game))
-    {:noreply, socket}
+    {{deck, _}, _, _} = socket.assigns.game
+    if len(deck) > 0 do
+      socket = assign(socket, :game, Game.turn(socket.assigns.game))
+      {:noreply, socket}
+    else
+      {:noreply, socket}
+    end
   end
   def handle_event("unselect", _, socket) do
     socket = assign(socket, :clicked, nil)
@@ -189,6 +109,7 @@ defmodule LVSolitaireWeb.LVSolitaireLive do
       IO.inspect(card)
       IO.inspect(params)
       IO.inspect(moves)
+      IO.inspect({t_idx , String.to_integer(index) , Atom.to_string(pile_to) , pile_type , card , clicked} )
       IO.inspect({t_idx == String.to_integer(index) , Atom.to_string(pile_to) == pile_type , card == clicked} )
       t_idx == String.to_integer(index) and Atom.to_string(pile_to) == pile_type and card == clicked
     end)
---
+++ lib/lvsolitaire_web/views/game_view.ex
@@ -0,0 +1,45 @@
+defmodule LVSolitaireWeb.GameView do
+  use LVSolitaireWeb, :view
+  use Phoenix.LiveView
+  import LVSolitaireWeb.LVSolitaireLive
+
+  def render_card(assigns, pile_type, {suit, rank} = card, index \\ nil) do 
+    ~L"""
+    <li>
+    <div phx-click="click"
+    phx-value-suit="<%= "#{suit}" %>"
+    phx-value-rank="<%= "#{rank}" %>"
+    phx-value-pile="<%= "#{pile_type}" %>"
+    phx-value-index="<%= "#{index}" %>"
+    class="card <%= focus?(assigns, card) %> rank-<%= num_to_rank( rank ) %> <%= suit %>">
+        <span class="rank"><%= num_to_rank(rank) %></span>
+        <span class="suit">&<%= suit %>;</span>
+      </div>
+    </li>
+    """
+  end
+  def render_card(assigns, :deck, :back, _) do 
+    ~L"""
+    <li><div phx-click="click-deck" class="card back">*</div></li>
+    """
+  end
+  def render_card(assigns, :tableau, :back, _) do 
+    ~L"""
+    <li><div class="card back">*</div></li>
+    """
+  end
+  def render_card(assigns, :foundation, _, index) do 
+    ~L"""
+    <li><div phx-click="click-empty"
+             phx-value-pile="foundation"
+             phx-value-index="<%= "#{index}" %>"
+             class="card">
+    </div></li>
+    """
+  end
+  def render_card(assigns, _, _, _) do 
+    ~L"""
+    <li><div class="card"></div></li>
+    """
+  end
+end
